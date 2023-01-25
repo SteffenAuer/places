73af8fcc7783b5caa8e56d1ae3246b6cc2b90e10
Steffen Auer <60623116+SteffenAuer@users.noreply.github.com>
2023-01-25 14:31:34 +0100

    add url and expanded button

--- src/components/Place/index.tsx
+++ src/components/Place/index.tsx
@@ -1,10 +1,13 @@
 import styled from '@emotion/styled';
+import { ExpandMoreRounded } from '@mui/icons-material';
 import {
   Card,
-  CardActionArea,
+  CardActions,
   CardContent,
   Collapse,
   Divider,
+  IconButton,
+  Link,
   Typography,
 } from '@mui/material';
 
@@ -12,13 +15,25 @@ import { useState } from 'react';
 import OpeningHours from '../OpeningHours';
 
 const StyledCard = styled(Card)`
-  margin: 0px 20px;
+  margin: 10px 20px;
   min-width: 400px;
 `;
 
+const StyledCardActions = styled(CardActions)`
+  justify-content: flex-end;
+  padding: 0px;
+`;
+
 const PhoneNumbersContainer = styled.div`
+  padding: 5px 0px;
   display: flex;
-  flex-flow: column wrap;
+  flex-flow: row wrap;
+  justify-content: space-between;
+`;
+
+const ExpandMoreButton = styled(IconButton)<{ expanded: boolean }>`
+  transform: ${({ expanded }) =>
+    expanded ? 'rotate(180deg)' : 'rotate(0deg)'};
 `;
 
 interface IPlaceProps {
@@ -32,35 +47,49 @@ const Place: React.FC<IPlaceProps> = ({ place }) => {
 
   const phoneNumbers = place.addresses?.[0]?.contacts
     ?.filter((contact) => contact.contact_type === 'phone')
-    .map((contact) => contact.formatted_service_code);
+    ?.map((contact) => contact.formatted_service_code);
+
+  const urlContact = place.addresses?.[0]?.contacts?.filter(
+    (contact) => contact.contact_type === 'url'
+  )?.[0];
 
   return (
     <StyledCard>
-      <CardActionArea onClick={toggleExpanded}>
-        <CardContent>
-          <Typography variant="h5">{place.name}</Typography>
+      <CardContent>
+        <Typography variant="h5">{place.name}</Typography>
 
-          <Typography variant="subtitle1" color="text.secondary">
-            {place.address}
-          </Typography>
+        <Typography variant="subtitle1" color="text.secondary">
+          {place.address}
+        </Typography>
+
+        <Link variant="subtitle2" href={urlContact.url}>
+          {urlContact.formatted_service_code}
+        </Link>
+
+        <StyledCardActions disableSpacing>
+          <ExpandMoreButton onClick={toggleExpanded} expanded={expanded}>
+            <ExpandMoreRounded />
+          </ExpandMoreButton>
+        </StyledCardActions>
+      </CardContent>
+
+      <Collapse in={expanded} timeout="auto">
+        <Divider variant="middle" />
+
+        <CardContent>
+          <Typography variant="h6">Contact</Typography>
 
           <PhoneNumbersContainer>
             {phoneNumbers.map((phoneNumber) => (
-              <Typography variant="body2">{phoneNumber}</Typography>
+              <Typography variant="body1">{phoneNumber}</Typography>
             ))}
           </PhoneNumbersContainer>
+
+          <Typography variant="h6">Opening Hours</Typography>
+
+          <OpeningHours place={place} />
         </CardContent>
-
-        <Collapse in={expanded} timeout="auto">
-          <Divider variant="middle" />
-
-          <CardContent>
-            <Typography variant="h6">Opening Hours</Typography>
-
-            <OpeningHours place={place} />
-          </CardContent>
-        </Collapse>
-      </CardActionArea>
+      </Collapse>
     </StyledCard>
   );
 };
